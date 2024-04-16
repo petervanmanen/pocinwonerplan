316d197ee3f5f35f3c6f5af31fb83874caf47b76
Peter van Manen <peter.van.manen@ritense.com>
2024-04-16 08:59:37 +0200

    improved some naming

--- src/main/java/nl/commutr/demo/DemoApplication.java
+++ src/main/java/nl/commutr/demo/DemoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InwonerplanApiApplication {
+public class DemoApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(InwonerplanApiApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 
